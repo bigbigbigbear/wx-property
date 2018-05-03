72694d3d477fa791d1b252c6c7d4cccbf72dc647
bigbigbigbear <308355212@qq.com>
2018-05-03 09:28:15 +0800

    update

--- src/server/index.js
+++ src/server/index.js
@@ -8,7 +8,7 @@ const Axios = axios.create({
   baseURL: baseUrl, // 因为我本地做了反向代理
   timeout: 7000,
   responseType: "json",
-  withCredentials: true, // 是否允许带cookie这些
+  withCredentials: false, // 是否允许带cookie这些
   headers: {
     "Content-Type": "application/x-www-form-urlencoded;charset=utf-8"
   }
